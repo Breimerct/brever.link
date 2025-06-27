bc9507f1e865d6ddd1ff247ddd09c0da25b2f5e6
Breimerct <breimerct@gmail.com>
2025-06-27 00:52:40 -0500

    feat: update site references and add favicon for brever.link

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  site: "https://brever.link",
 
   vite: {
     plugins: [tailwindcss()],
